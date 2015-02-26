13e99ef4baf6a5f934db0a120c1ae04d40b44605
rdillin <rdillin@oaklawn.com>
2015-02-26 14:48:05 -0600

    More distinguishable separation of types due to color?

--- src/StepD.Designer.vb
+++ src/StepD.Designer.vb
@@ -23,12 +23,16 @@ Partial Class StepD
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
 		Me.Panel2 = New System.Windows.Forms.Panel()
+		Me.RadioButton1 = New System.Windows.Forms.RadioButton()
 		Me.RadioButton7 = New System.Windows.Forms.RadioButton()
 		Me.TextBox2 = New System.Windows.Forms.TextBox()
 		Me.RadioButton6 = New System.Windows.Forms.RadioButton()
 		Me.RadioButton3 = New System.Windows.Forms.RadioButton()
 		Me.RadioButton2 = New System.Windows.Forms.RadioButton()
 		Me.Label2 = New System.Windows.Forms.Label()
+		Me.Panel1 = New System.Windows.Forms.Panel()
+		Me.Panel3 = New System.Windows.Forms.Panel()
+		Me.Panel4 = New System.Windows.Forms.Panel()
 		Me.Panel2.SuspendLayout()
 		Me.SuspendLayout()
 		'
@@ -40,32 +44,50 @@ Partial Class StepD
 		'Panel2
 		'
 		Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.Panel2.Controls.Add(Me.RadioButton1)
 		Me.Panel2.Controls.Add(Me.RadioButton7)
 		Me.Panel2.Controls.Add(Me.TextBox2)
 		Me.Panel2.Controls.Add(Me.RadioButton6)
 		Me.Panel2.Controls.Add(Me.RadioButton3)
 		Me.Panel2.Controls.Add(Me.RadioButton2)
 		Me.Panel2.Controls.Add(Me.Label2)
+		Me.Panel2.Controls.Add(Me.Panel1)
+		Me.Panel2.Controls.Add(Me.Panel3)
+		Me.Panel2.Controls.Add(Me.Panel4)
 		Me.Panel2.Location = New System.Drawing.Point(41, 56)
 		Me.Panel2.Name = "Panel2"
-		Me.Panel2.Size = New System.Drawing.Size(514, 177)
+		Me.Panel2.Size = New System.Drawing.Size(514, 209)
 		Me.Panel2.TabIndex = 3
+		'
+		'RadioButton1
+		'
+		Me.RadioButton1.AutoSize = True
+		Me.RadioButton1.BackColor = System.Drawing.Color.PaleGreen
+		Me.RadioButton1.Location = New System.Drawing.Point(23, 77)
+		Me.RadioButton1.Name = "RadioButton1"
+		Me.RadioButton1.Size = New System.Drawing.Size(148, 17)
+		Me.RadioButton1.TabIndex = 11
+		Me.RadioButton1.Text = "Single Event: Payout Only"
+		Me.RadioButton1.UseVisualStyleBackColor = False
 		'
 		'RadioButton7
 		'
 		Me.RadioButton7.AutoSize = True
-		Me.RadioButton7.Location = New System.Drawing.Point(3, 97)
+		Me.RadioButton7.BackColor = System.Drawing.Color.PaleGreen
+		Me.RadioButton7.Checked = True
+		Me.RadioButton7.Location = New System.Drawing.Point(23, 31)
 		Me.RadioButton7.Name = "RadioButton7"
-		Me.RadioButton7.Size = New System.Drawing.Size(143, 17)
+		Me.RadioButton7.Size = New System.Drawing.Size(172, 17)
 		Me.RadioButton7.TabIndex = 10
 		Me.RadioButton7.TabStop = True
-		Me.RadioButton7.Text = "Mult-Part Single Instance"
-		Me.RadioButton7.UseVisualStyleBackColor = True
+		Me.RadioButton7.Text = "Single Event: Entry and Payout"
+		Me.RadioButton7.UseVisualStyleBackColor = False
 		'
 		'TextBox2
 		'
+		Me.TextBox2.BackColor = System.Drawing.SystemColors.HighlightText
 		Me.TextBox2.Enabled = False
-		Me.TextBox2.Location = New System.Drawing.Point(181, 127)
+		Me.TextBox2.Location = New System.Drawing.Point(266, 121)
 		Me.TextBox2.Name = "TextBox2"
 		Me.TextBox2.Size = New System.Drawing.Size(115, 20)
 		Me.TextBox2.TabIndex = 9
@@ -74,35 +96,35 @@ Partial Class StepD
 		'RadioButton6
 		'
 		Me.RadioButton6.AutoSize = True
-		Me.RadioButton6.Location = New System.Drawing.Point(3, 127)
+		Me.RadioButton6.BackColor = System.Drawing.Color.PaleTurquoise
+		Me.RadioButton6.Location = New System.Drawing.Point(23, 121)
 		Me.RadioButton6.Name = "RadioButton6"
-		Me.RadioButton6.Size = New System.Drawing.Size(171, 17)
+		Me.RadioButton6.Size = New System.Drawing.Size(240, 17)
 		Me.RadioButton6.TabIndex = 8
-		Me.RadioButton6.TabStop = True
-		Me.RadioButton6.Text = "Multi-Part Sequential Instances"
-		Me.RadioButton6.UseVisualStyleBackColor = True
+		Me.RadioButton6.Text = "Multi-Part Sequential Event: Entry and Payout"
+		Me.RadioButton6.UseVisualStyleBackColor = False
 		'
 		'RadioButton3
 		'
 		Me.RadioButton3.AutoSize = True
-		Me.RadioButton3.Location = New System.Drawing.Point(3, 66)
+		Me.RadioButton3.BackColor = System.Drawing.Color.Lavender
+		Me.RadioButton3.Location = New System.Drawing.Point(23, 170)
 		Me.RadioButton3.Name = "RadioButton3"
 		Me.RadioButton3.Size = New System.Drawing.Size(76, 17)
 		Me.RadioButton3.TabIndex = 6
 		Me.RadioButton3.Text = "Acquisition"
-		Me.RadioButton3.UseVisualStyleBackColor = True
+		Me.RadioButton3.UseVisualStyleBackColor = False
 		'
 		'RadioButton2
 		'
 		Me.RadioButton2.AutoSize = True
-		Me.RadioButton2.Checked = True
-		Me.RadioButton2.Location = New System.Drawing.Point(3, 36)
+		Me.RadioButton2.BackColor = System.Drawing.Color.PaleGreen
+		Me.RadioButton2.Location = New System.Drawing.Point(23, 54)
 		Me.RadioButton2.Name = "RadioButton2"
-		Me.RadioButton2.Size = New System.Drawing.Size(62, 17)
+		Me.RadioButton2.Size = New System.Drawing.Size(139, 17)
 		Me.RadioButton2.TabIndex = 5
-		Me.RadioButton2.TabStop = True
-		Me.RadioButton2.Text = "General"
-		Me.RadioButton2.UseVisualStyleBackColor = True
+		Me.RadioButton2.Text = "Single Event: Entry Only"
+		Me.RadioButton2.UseVisualStyleBackColor = False
 		'
 		'Label2
 		'
@@ -113,6 +135,33 @@ Partial Class StepD
 		Me.Label2.Size = New System.Drawing.Size(247, 16)
 		Me.Label2.TabIndex = 3
 		Me.Label2.Text = "What is the type of the new promo?"
+		'
+		'Panel1
+		'
+		Me.Panel1.BackColor = System.Drawing.Color.PaleGreen
+		Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.Panel1.Location = New System.Drawing.Point(3, 23)
+		Me.Panel1.Name = "Panel1"
+		Me.Panel1.Size = New System.Drawing.Size(506, 78)
+		Me.Panel1.TabIndex = 12
+		'
+		'Panel3
+		'
+		Me.Panel3.BackColor = System.Drawing.Color.PaleTurquoise
+		Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.Panel3.Location = New System.Drawing.Point(3, 107)
+		Me.Panel3.Name = "Panel3"
+		Me.Panel3.Size = New System.Drawing.Size(506, 43)
+		Me.Panel3.TabIndex = 13
+		'
+		'Panel4
+		'
+		Me.Panel4.BackColor = System.Drawing.Color.Lavender
+		Me.Panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.Panel4.Location = New System.Drawing.Point(3, 156)
+		Me.Panel4.Name = "Panel4"
+		Me.Panel4.Size = New System.Drawing.Size(506, 43)
+		Me.Panel4.TabIndex = 14
 		'
 		'StepD
 		'
@@ -137,6 +186,10 @@ Partial Class StepD
     Friend WithEvents RadioButton6 As System.Windows.Forms.RadioButton
     Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
     Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
-    Friend WithEvents Label2 As System.Windows.Forms.Label
+	Friend WithEvents Label2 As System.Windows.Forms.Label
+	Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+	Friend WithEvents Panel1 As System.Windows.Forms.Panel
+	Friend WithEvents Panel3 As System.Windows.Forms.Panel
+	Friend WithEvents Panel4 As System.Windows.Forms.Panel
 
 End Class
