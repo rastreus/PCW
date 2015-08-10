97dd81b71d07b13ec1db63fc0c58e957e48f9aea
rdillin <rdillin@oaklawn.com>
2015-08-10 12:59:37 -0500

    Cleaned up PAE (Design, programming style)

--- src/PAE.Designer.vb
+++ src/PAE.Designer.vb
@@ -22,129 +22,153 @@ Partial Class PAE
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Panel3 = New System.Windows.Forms.Panel()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Panel2 = New System.Windows.Forms.Panel()
-        Me.Panel1.SuspendLayout()
-        Me.Panel3.SuspendLayout()
-        Me.Panel2.SuspendLayout()
-        Me.SuspendLayout()
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.ForeColor = System.Drawing.Color.Chocolate
-        Me.Label1.Location = New System.Drawing.Point(41, 19)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(468, 39)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "PromotionalAlterationEditor"
-        '
-        'Label2
-        '
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.ForeColor = System.Drawing.Color.SaddleBrown
-        Me.Label2.Location = New System.Drawing.Point(48, 76)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(502, 65)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "The purpose of this tool is to allow minor changes to existing promos. If a lot o" & _
-    "f modifications are needed, then you are advised to cancel this operation and st" & _
-    "art PCW to create a new promo instead."
-        '
-        'Panel1
-        '
-        Me.Panel1.BackColor = System.Drawing.SystemColors.Control
-        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.Panel1.Controls.Add(Me.Panel3)
-        Me.Panel1.Controls.Add(Me.Button2)
-        Me.Panel1.Controls.Add(Me.Button1)
-        Me.Panel1.Location = New System.Drawing.Point(-5, 798)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(601, 40)
-        Me.Panel1.TabIndex = 2
-        '
-        'Panel3
-        '
-        Me.Panel3.BackColor = System.Drawing.SystemColors.Control
-        Me.Panel3.Controls.Add(Me.CheckBox1)
-        Me.Panel3.Location = New System.Drawing.Point(128, 3)
-        Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(377, 32)
-        Me.Panel3.TabIndex = 5
-        '
-        'CheckBox1
-        '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(7, 8)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(367, 17)
-        Me.CheckBox1.TabIndex = 4
-        Me.CheckBox1.Text = "I understand and take responsibility for the changes to the above promo."
-        Me.CheckBox1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(511, 7)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 4
-        Me.Button2.Text = "Submit"
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(16, 7)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 4
-        Me.Button1.Text = "Cancel"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Panel2
-        '
-        Me.Panel2.BackColor = System.Drawing.Color.White
-        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.Panel2.Controls.Add(Me.Label1)
-        Me.Panel2.Controls.Add(Me.Label2)
-        Me.Panel2.Location = New System.Drawing.Point(-5, -1)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(601, 160)
-        Me.Panel2.TabIndex = 3
-        '
-        'PAE
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.Color.Cornsilk
-        Me.ClientSize = New System.Drawing.Size(594, 837)
-        Me.Controls.Add(Me.Panel2)
-        Me.Controls.Add(Me.Panel1)
-        Me.MaximizeBox = False
-        Me.Name = "PAE"
-        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
-        Me.Text = "PAE"
-        Me.Panel1.ResumeLayout(False)
-        Me.Panel3.ResumeLayout(False)
-        Me.Panel3.PerformLayout()
-        Me.Panel2.ResumeLayout(False)
-        Me.Panel2.PerformLayout()
-        Me.ResumeLayout(False)
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(PAE))
+		Me.lblPAE = New System.Windows.Forms.Label()
+		Me.lblDescription = New System.Windows.Forms.Label()
+		Me.pnlControl = New System.Windows.Forms.Panel()
+		Me.pnlControlDarkDark = New System.Windows.Forms.Panel()
+		Me.cbResponsibility = New System.Windows.Forms.CheckBox()
+		Me.btnSubmit = New System.Windows.Forms.Button()
+		Me.btnCancel = New System.Windows.Forms.Button()
+		Me.pnlSaddleBrown = New System.Windows.Forms.Panel()
+		Me.pnlControl.SuspendLayout()
+		Me.pnlControlDarkDark.SuspendLayout()
+		Me.pnlSaddleBrown.SuspendLayout()
+		Me.SuspendLayout()
+		'
+		'lblPAE
+		'
+		Me.lblPAE.AutoSize = True
+		Me.lblPAE.BackColor = System.Drawing.Color.Transparent
+		Me.lblPAE.CausesValidation = False
+		Me.lblPAE.Font = New System.Drawing.Font("Consolas", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lblPAE.ForeColor = System.Drawing.Color.Tomato
+		Me.lblPAE.Location = New System.Drawing.Point(41, 19)
+		Me.lblPAE.Name = "lblPAE"
+		Me.lblPAE.Size = New System.Drawing.Size(503, 37)
+		Me.lblPAE.TabIndex = 0
+		Me.lblPAE.Text = "PromotionalAlterationEditor"
+		Me.lblPAE.UseMnemonic = False
+		'
+		'lblDescription
+		'
+		Me.lblDescription.BackColor = System.Drawing.Color.Transparent
+		Me.lblDescription.CausesValidation = False
+		Me.lblDescription.Font = New System.Drawing.Font("Consolas", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.lblDescription.ForeColor = System.Drawing.Color.White
+		Me.lblDescription.Location = New System.Drawing.Point(16, 76)
+		Me.lblDescription.Name = "lblDescription"
+		Me.lblDescription.Size = New System.Drawing.Size(568, 64)
+		Me.lblDescription.TabIndex = 0
+		Me.lblDescription.Text = "The purpose of this tool is to allow minor changes to existing promos. If a lot o" & _
+	"f modifications are needed, then you are advised to cancel this operation and st" & _
+	"art PCW to create a new promo instead."
+		Me.lblDescription.UseMnemonic = False
+		'
+		'pnlControl
+		'
+		Me.pnlControl.BackColor = System.Drawing.SystemColors.Control
+		Me.pnlControl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.pnlControl.CausesValidation = False
+		Me.pnlControl.Controls.Add(Me.pnlControlDarkDark)
+		Me.pnlControl.Controls.Add(Me.btnSubmit)
+		Me.pnlControl.Controls.Add(Me.btnCancel)
+		Me.pnlControl.Location = New System.Drawing.Point(-5, 798)
+		Me.pnlControl.Name = "pnlControl"
+		Me.pnlControl.Size = New System.Drawing.Size(601, 40)
+		Me.pnlControl.TabIndex = 2
+		'
+		'pnlControlDarkDark
+		'
+		Me.pnlControlDarkDark.BackColor = System.Drawing.SystemColors.ControlDarkDark
+		Me.pnlControlDarkDark.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.pnlControlDarkDark.CausesValidation = False
+		Me.pnlControlDarkDark.Controls.Add(Me.cbResponsibility)
+		Me.pnlControlDarkDark.ForeColor = System.Drawing.Color.White
+		Me.pnlControlDarkDark.Location = New System.Drawing.Point(97, 3)
+		Me.pnlControlDarkDark.Name = "pnlControlDarkDark"
+		Me.pnlControlDarkDark.Size = New System.Drawing.Size(408, 32)
+		Me.pnlControlDarkDark.TabIndex = 0
+		'
+		'cbResponsibility
+		'
+		Me.cbResponsibility.CausesValidation = False
+		Me.cbResponsibility.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.cbResponsibility.Location = New System.Drawing.Point(3, -2)
+		Me.cbResponsibility.Name = "cbResponsibility"
+		Me.cbResponsibility.Size = New System.Drawing.Size(398, 32)
+		Me.cbResponsibility.TabIndex = 0
+		Me.cbResponsibility.TabStop = False
+		Me.cbResponsibility.Text = "I take responsibility for the changes to the above promo."
+		Me.cbResponsibility.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		Me.cbResponsibility.UseMnemonic = False
+		Me.cbResponsibility.UseVisualStyleBackColor = True
+		'
+		'btnSubmit
+		'
+		Me.btnSubmit.CausesValidation = False
+		Me.btnSubmit.Location = New System.Drawing.Point(511, 7)
+		Me.btnSubmit.Name = "btnSubmit"
+		Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+		Me.btnSubmit.TabIndex = 0
+		Me.btnSubmit.TabStop = False
+		Me.btnSubmit.Text = "Submit"
+		Me.btnSubmit.UseMnemonic = False
+		Me.btnSubmit.UseVisualStyleBackColor = True
+		'
+		'btnCancel
+		'
+		Me.btnCancel.CausesValidation = False
+		Me.btnCancel.Location = New System.Drawing.Point(16, 7)
+		Me.btnCancel.Name = "btnCancel"
+		Me.btnCancel.Size = New System.Drawing.Size(75, 23)
+		Me.btnCancel.TabIndex = 0
+		Me.btnCancel.TabStop = False
+		Me.btnCancel.Text = "Cancel"
+		Me.btnCancel.UseMnemonic = False
+		Me.btnCancel.UseVisualStyleBackColor = True
+		'
+		'pnlSaddleBrown
+		'
+		Me.pnlSaddleBrown.BackColor = System.Drawing.Color.SaddleBrown
+		Me.pnlSaddleBrown.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.pnlSaddleBrown.CausesValidation = False
+		Me.pnlSaddleBrown.Controls.Add(Me.lblPAE)
+		Me.pnlSaddleBrown.Controls.Add(Me.lblDescription)
+		Me.pnlSaddleBrown.Location = New System.Drawing.Point(-5, -1)
+		Me.pnlSaddleBrown.Name = "pnlSaddleBrown"
+		Me.pnlSaddleBrown.Size = New System.Drawing.Size(601, 160)
+		Me.pnlSaddleBrown.TabIndex = 0
+		'
+		'PAE
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.Color.Peru
+		Me.CausesValidation = False
+		Me.ClientSize = New System.Drawing.Size(592, 837)
+		Me.Controls.Add(Me.pnlSaddleBrown)
+		Me.Controls.Add(Me.pnlControl)
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+		Me.MaximizeBox = False
+		Me.Name = "PAE"
+		Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+		Me.Text = "PAE (PromotionalAlternationEditor)"
+		Me.pnlControl.ResumeLayout(False)
+		Me.pnlControlDarkDark.ResumeLayout(False)
+		Me.pnlSaddleBrown.ResumeLayout(False)
+		Me.pnlSaddleBrown.PerformLayout()
+		Me.ResumeLayout(False)
 
-    End Sub
-    Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents Label2 As System.Windows.Forms.Label
-    Friend WithEvents Panel1 As System.Windows.Forms.Panel
-    Friend WithEvents Panel2 As System.Windows.Forms.Panel
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Button2 As System.Windows.Forms.Button
-    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
-    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+	End Sub
+	Private WithEvents pnlControl As System.Windows.Forms.Panel
+	Private WithEvents btnCancel As System.Windows.Forms.Button
+	Private WithEvents btnSubmit As System.Windows.Forms.Button
+	Private WithEvents cbResponsibility As System.Windows.Forms.CheckBox
+	Private WithEvents pnlControlDarkDark As System.Windows.Forms.Panel
+	Private WithEvents pnlSaddleBrown As System.Windows.Forms.Panel
+	Private WithEvents lblPAE As System.Windows.Forms.Label
+	Private WithEvents lblDescription As System.Windows.Forms.Label
 End Class
--- src/PAE.vb
+++ src/PAE.vb
@@ -1,22 +1,29 @@
 ﻿Public Class PAE
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim cancel As Boolean = AskToClose()
-        If cancel Then
-            Me.Close()
-        End If
-    End Sub
+	Private Sub btnCancel_Click(sender As Object, _
+								e As EventArgs) _
+		Handles btnCancel.Click
+		Dim cancel As Boolean = AskToClose()
+		If cancel = True Then
+			Me.Close()
+		End If
+	End Sub
 
-    Private Function AskToClose()
-        Dim cancelMsgString As String = <a>Do you wish to quit the editor now?
-Your changes will not be saved if you do.</a>.Value
-
-        Dim result As Integer = CenteredMessagebox.MsgBox.Show(cancelMsgString, "Exit editor?", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
-
-        If result = DialogResult.Yes Then
-            Return True
-        Else
-            Return False
-        End If
-    End Function
+	Private Function AskToClose()
+		Dim bool As Boolean = False
+		Dim cancelMsgString As String = "Do you wish to quit PAE now?" & _
+										vbCrLf & "Your changes will not " & _
+										"be saved if you do."
+		Dim result As Integer = _
+			CenteredMessagebox.MsgBox.Show(cancelMsgString, _
+										   "Exit editor?", _
+											MessageBoxButtons.YesNo, _
+											MessageBoxIcon.Question)
+		If result = DialogResult.Yes Then
+			bool = True
+		Else
+			bool = False
+		End If
+		Return bool
+	End Function
 End Class
