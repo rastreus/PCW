f0f0d2571bed832058cb02188bf401b82079741d
rdillin <rdillin@oaklawn.com>
2015-08-10 12:26:58 -0500

    frmStartup: Added Step-by-Step instructions, comments

--- src/frmStartup.Designer.vb
+++ src/frmStartup.Designer.vb
@@ -22,26 +22,30 @@ Partial Class frmStartup
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmStartup))
 		Me.SuspendLayout()
 		'
 		'frmStartup
 		'
-		Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 22.0!)
-		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
 		Me.BackColor = System.Drawing.Color.Lime
 		Me.CausesValidation = False
 		Me.ClientSize = New System.Drawing.Size(173, 124)
+		Me.ControlBox = False
 		Me.Font = New System.Drawing.Font("Consolas", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
 		Me.ForeColor = System.Drawing.Color.Black
 		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
-		Me.Margin = New System.Windows.Forms.Padding(5, 5, 5, 5)
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+		Me.Margin = New System.Windows.Forms.Padding(5)
 		Me.MaximizeBox = False
 		Me.MinimizeBox = False
 		Me.Name = "frmStartup"
 		Me.Opacity = 0.0R
+		Me.ShowIcon = False
 		Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
 		Me.Text = "frmStartup"
 		Me.TransparencyKey = System.Drawing.Color.Lime
+		Me.UseWaitCursor = True
 		Me.ResumeLayout(False)
 
 	End Sub
--- src/frmStartup.vb
+++ src/frmStartup.vb
@@ -1,18 +1,37 @@
 ﻿Imports AutoUpdate
 
+''' <summary>
+''' Startup Form which checks for updates (and applies if needed).
+''' </summary>
+''' <remarks>
+''' On each new release of PCW that needs to be pushed out:
+''' 0.) Go to the RootPath.
+''' 1.) Create a new folder whose name is the newest version.
+'''     EXAMPLE: 00040001
+''' 2.) Copy the new release exe into the folder from \bin\Release.
+''' 3.) Rename the exe by appending "-" and the newest version.
+'''     EXAMPLE: PromotionalCreationWizard-00040001.exe
+''' 4.) Update the "UpdateFile" with the newest version.
+'''     EXAMPLE: 0.4.0.1
+''' 5.) Done! An old, out-of-date copy of PCW will auto-update itself.
+''' </remarks>
 Public Class frmStartup
 	Private Sub frmStartup_Load(sender As Object, _
 								e As EventArgs) _
 		Handles Me.Load
 		AutoUpdate.RootPath = "\\domainserver\data\InformationTechnology" & _
 							  "\Software Master\Oaklawn\PCW"
-		AutoUpdate.ErrorMessage = "THERE WAS AN ERROR IN AUTOUPDATE!"
+		AutoUpdate.UpdateFile = "_update.dat"
+		AutoUpdate.ErrorMessage = "ERROR IN AUTOUPDATE!"
 		If AutoUpdate.UpdateEXE() Then
 			GUI_Util.msgBox("PCW auto-updated to latest version!" & vbCrLf & _
 							"Please restart PCW after quit.", _
 							"AUTO-UPDATE SUCCESS!", _
 							"Information")
 		Else
+			'Eventually this will be frmSelect
+			'instead of going to PCW directly.
+			'(GRD;08/10/15)
 			Dim frmMain As PCW = New PCW
 			frmMain.Show()
 		End If
