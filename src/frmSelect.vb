cb36b1b5603a0303328b013361a56c83f417b276
rdillin <rdillin@oaklawn.com>
2015-08-10 11:51:07 -0500

    Added frmSelect (PCW or PAE)

---
+++ src/frmSelect.vb
@@ -0,0 +1,25 @@
+﻿''' <summary>
+''' Simple Form which opens PCW or PAE.
+''' </summary>
+''' <remarks>
+''' On "Me.Close()" the ""Me" is frmSelect. 
+''' Application will not quit here because
+''' "Shutdown mode" is set to "When last form closes."
+''' </remarks>
+Public Class frmSelect
+	Private Sub btnOpenPCW_Click(sender As Object, _
+								 e As EventArgs) _
+		Handles btnOpenPCW.Click
+		Dim frmWizard As PCW = New PCW
+		frmWizard.Show()
+		Me.Close()
+	End Sub
+
+	Private Sub btnOpenPAE_Click(sender As Object, _
+								 e As EventArgs) _
+		Handles btnOpenPAE.Click
+		Dim frmEditor As PAE = New PAE
+		frmEditor.Show()
+		Me.Close()
+	End Sub
+End Class
